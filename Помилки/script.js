518da60e494313bc69752aef0573d366373e49c6
Andriy <pereginets1995@gmail.com>
2023-04-24 16:49:33 +0300

    try-catch

---
+++ Помилки/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const data = [
+    {
+        id: 'box',
+        tag: 'div'
+    },
+    {
+        id: '',
+        tag: 'nav'
+    },
+    {
+        id: 'circle',
+        tag: 'span'
+    }
+];
+
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+    
+        if (!blockObj.id) {
+            throw new SyntaxError(`в даних під номером ${i+1} немає ніхуя!`);
+        } 
+    
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    });
+} catch(e) {
+    if (e.name === 'SyntaxError') {
+        console.log(e.message);
+    } else {
+        throw e;
+    }
+
+
+}
+
+const err = new SyntaxError(';bmfknmkfgm');
+
